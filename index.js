efe605dc6f9c0912813f1a252487e0d23bc924ef
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2023-10-30 14:52:02 +0700

    deploy3

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Success" });
-});
-app.get("/users", (req, res) => {
-  return res.send("Daftar User");
-});
-
-app.listen(3007, () => {
-  console.log("Process running at port 3007");
-});
+require("./bin/www");
