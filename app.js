e0753a026334e9b69a44a826e125231af27ac209
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2023-10-30 22:24:41 +0700

    deploy30okt19

--- app.js
+++ app.js
@@ -16,7 +16,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// home
+app.use("/", function (req, res) {
+  res.render("index", {
+    title: "CafeApp API Service",
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
