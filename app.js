65c6f3e806cb2fb9cf38f35c85cb88af87be48d7
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2023-10-31 13:39:08 +0700

    deploy31okt2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const productRoute = require("./app/product/router");
+// const productRoute = require("./app/product/router");
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api", productRoute);
+// app.use("/api", productRoute);
 
 // home
 app.use("/", function (req, res) {
@@ -28,19 +28,19 @@ app.use("/", function (req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
