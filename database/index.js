d55dbe3c3b3672fd205e7215c3c88ffd1850997e
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2023-10-31 14:03:01 +0700

    deploy31okt3

--- database/index.js
+++ database/index.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 // const { dbHost, dbPass, dbName, dbUser, dbPort } = require("../app/config");
 mongoose.set("strictQuery", false);
-mongoose.connect(
-  `mongodb://user_latihan:123456@127.0.0.1:27017/eduworkstore?authSource=admin`
-);
+// mongoose.connect(
+//   `mongodb://user_latihan:123456@127.0.0.1:27017/eduworkstore?authSource=admin`
+// );
 // mongoose.connect(
 //   `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
 // );
-// mongoose.connect(
-//   `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?retryWrites=true&w=majority`
-// );
+mongoose.connect(
+  `mongodb+srv://mochamaddwipamungkas:12131213@dwi-mongodbatlas.gywboqh.mongodb.net/eduworkstore?retryWrites=true&w=majority`
+);
 const db = mongoose.connection;
 
 module.exports = db;
